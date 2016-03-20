67cd89a1d4643a92631846c85deed2971c00071c
tichise <ichise@gmail.com>
2016-03-20 13:51:09 +0900

    add

---
+++ Sources/Entry.swift
@@ -0,0 +1,26 @@
+//
+//  Entry.swift
+//
+//  Created by tichise on 2016/03/17.
+//  Copyright © 2016年 tichise. All rights reserved.
+//
+
+import Foundation
+
+public struct Entry {
+
+    public internal(set) var id:String?
+    public internal(set) var title:String?
+    public internal(set) var link:String?
+    public internal(set) var updated:NSDate?
+    public internal(set) var summary:String?
+    
+    init(id:String, title:String, link:String, updated:NSDate, summary:String){
+
+        self.id = id
+        self.title = title
+        self.link = link
+        self.updated = updated
+        self.summary = summary
+    }
+}
