c0056fbd8724f1ee8175832895ff004f8d1e146e
tichise <ichise@pepabo.com>
2018-12-13 11:15:55 +0900

    updatedをDate型に変更

--- Sources/Entry.swift
+++ Sources/Entry.swift
@@ -12,7 +12,7 @@ public struct Entry {
     public internal(set) var id:String?
     public internal(set) var title:String?
     public internal(set) var link:String?
-    public internal(set) var updated:NSDate?
+    public internal(set) var updated:Date?
     public internal(set) var summary:String?
     
     init(id:String, title:String, link:String, updated:NSDate?, summary:String?){
