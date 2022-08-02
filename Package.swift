1b9baeb0b4ad5b31f6970f06209b74f135625206
tichise <43707+tichise@users.noreply.github.com>
2022-08-02 15:01:53 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-  name: "TIFeedParser"
+    name: "TIFeedParser",
+    platforms: [.iOS(.v10),
+                .watchOS(.v3)],
+    products: [
+        .library(name: "TIFeedParser", targets: ["TIFeedParser"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TIFeedParser")
+    ],
+    swiftLanguageVersions: [.v5]
 )
