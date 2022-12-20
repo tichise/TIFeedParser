d60dd15dfa025db4f7c20a67a8b888a7c26352ff
tichise <43707+tichise@users.noreply.github.com>
2022-12-20 20:47:48 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.1"),
-        .package(url: "https://github.com/malcommac/SwiftDate", from: "6.3.1"),
+        .package(url: "https://github.com/malcommac/SwiftDate", from: "7.0.0"),
     ],
     targets: [
         .target(name: "TIFeedParser", dependencies: ["AEXML", "SwiftDate"], path: "Sources"),
